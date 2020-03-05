cfd5e9f134e58477b5bb9ac553fe5d62f05274dc
Jun Yi Hoo <junyihoo1997@gmail.com>
2020-03-05 12:53:06 +0800

    Updates

---
+++ precache-manifest.1e8f1b06e6ad3a1b1f27ec877e121ef3.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de78d19e3f0e3aba99e092de15b99cda",
+    "url": "/static-sidejob-sk/index.html"
+  },
+  {
+    "revision": "29aef1e47e1daec2cc62",
+    "url": "/static-sidejob-sk/static/css/2.5fa35e15.chunk.css"
+  },
+  {
+    "revision": "d589a665849fdb0834ed",
+    "url": "/static-sidejob-sk/static/css/main.293ecca1.chunk.css"
+  },
+  {
+    "revision": "29aef1e47e1daec2cc62",
+    "url": "/static-sidejob-sk/static/js/2.28d84c1e.chunk.js"
+  },
+  {
+    "revision": "d589a665849fdb0834ed",
+    "url": "/static-sidejob-sk/static/js/main.4a98bd22.chunk.js"
+  },
+  {
+    "revision": "bd13751d240349871c5a",
+    "url": "/static-sidejob-sk/static/js/runtime~main.f9ff3e0f.js"
+  },
+  {
+    "revision": "96921b10e5cde475fb88d4fc6a75e1b2",
+    "url": "/static-sidejob-sk/static/media/Earing2.96921b10.jpg"
+  },
+  {
+    "revision": "f007e50520219cd70547b0b794c7e205",
+    "url": "/static-sidejob-sk/static/media/Facebook.f007e505.png"
+  },
+  {
+    "revision": "ed28a4b02f68700225afb1e594eb497f",
+    "url": "/static-sidejob-sk/static/media/Instagramlogo.ed28a4b0.png"
+  },
+  {
+    "revision": "56e1ed34ab9c32fbd0c29034dda06e0d",
+    "url": "/static-sidejob-sk/static/media/Login.56e1ed34.PNG"
+  },
+  {
+    "revision": "bec15a0aa9c5773942ae066f8caaa02f",
+    "url": "/static-sidejob-sk/static/media/Ring.bec15a0a.jpg"
+  },
+  {
+    "revision": "94abb852c81bfc2b2f3168458a6d3ad3",
+    "url": "/static-sidejob-sk/static/media/Ring2.94abb852.jpg"
+  },
+  {
+    "revision": "5b764988f49b9b92cf989adc1ef536c3",
+    "url": "/static-sidejob-sk/static/media/Ring3.5b764988.jpg"
+  },
+  {
+    "revision": "9c5e8b193295f4fc43741f2c25acdd7d",
+    "url": "/static-sidejob-sk/static/media/Ring5.9c5e8b19.jpg"
+  },
+  {
+    "revision": "3bff56a91521a0676b8f942f585db6d6",
+    "url": "/static-sidejob-sk/static/media/Wishlist.3bff56a9.png"
+  },
+  {
+    "revision": "df2f0b10e0db7b5093d58aee3e97ec78",
+    "url": "/static-sidejob-sk/static/media/bracelets2.df2f0b10.jpg"
+  },
+  {
+    "revision": "9eddb98719160cd4e59bc289bbe22680",
+    "url": "/static-sidejob-sk/static/media/dummy+logo.9eddb987.jpg"
+  },
+  {
+    "revision": "568523117dedf90f155ac56ccb77e154",
+    "url": "/static-sidejob-sk/static/media/location.56852311.png"
+  },
+  {
+    "revision": "6da9d39faa5bc01505d4a3dcf18f000d",
+    "url": "/static-sidejob-sk/static/media/watch2.6da9d39f.jpg"
+  }
+]);
